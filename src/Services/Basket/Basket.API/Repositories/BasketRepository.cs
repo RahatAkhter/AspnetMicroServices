3b7d64f5378dc81fce9a10cced74b63afe26d21c
Rahat Akhter <rahat.akhter@itsolution24x7.net>
2022-02-02 20:10:36 +0500

    csdcsdc

---
+++ src/Services/Basket/Basket.API/Repositories/BasketRepository.cs
@@ -0,0 +1,48 @@
+﻿using Basket.API.Entities;
+using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Basket.API.Repositories
+{
+    public class BasketRepository : IBasketRepository
+    {
+        private readonly IDistributedCache _redisCache;
+
+        public BasketRepository(IDistributedCache cache)
+        {
+            _redisCache = cache ?? throw new ArgumentNullException(nameof (cache));
+        }
+        public async Task<ShoppingCart> GetBasket(string userName)
+        {
+            var basket = await _redisCache.GetStringAsync(userName);
+            if (string.IsNullOrEmpty(basket))
+                return null;
+            return JsonConvert.DeserializeObject<ShoppingCart>(basket);
+        }
+
+        public async Task<ShoppingCart> UpdateBasket(ShoppingCart basket)
+        {
+            try
+            {
+                var abc = JsonConvert.SerializeObject(basket);
+                await _redisCache.SetStringAsync(basket.UserName,abc);
+                return await GetBasket(basket.UserName);
+            }
+            catch(Exception ex)
+            {
+                return null;
+            }
+        }
+
+        public async Task DeleteBasket(string userName)
+        {
+            await _redisCache.RemoveAsync(userName);
+        }
+
+         
+    }
+}
